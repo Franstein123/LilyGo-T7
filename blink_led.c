d63000ac463c4b37ad87055925f02f36e095e87f
Franstein <fransharkonen@gmail.com>
2023-05-26 17:31:15 +0300

    testing

---
+++ blink_led.c
@@ -0,0 +1,10 @@
+/*
+*
+* This is my first project to make my LilyGo T7 blink it's led.
+* Started on 22.5.2023 after setting up SSH-connection to my linux laptop.
+* First I should download needed expansions for lilygo and see it's manual...
+*
+* Made by Frans Härkönen
+*
+*/
+
